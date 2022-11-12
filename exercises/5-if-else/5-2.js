09f1ac22bd5d247e70566d451d960e809a49dbeb
Maxime Bastien <maxime.bastien@student.ehb.be>
2022-11-12 23:32:06 +0100

    5-2 colored maze

--- exercises/5-if-else/5-2.js
+++ exercises/5-if-else/5-2.js
@@ -14,12 +14,13 @@ function drawMaze(){
 
             let rand = Math.round(Math.random());
             context.lineWidth= 5;
+            context.strokeStyle = Utils.rgb();
             console.log(rand);
 
             if(rand == 0){
-                Utils.drawLine(-2 + i * 10, -2 + j * 10, 10 + i * 10, 10 + j * 10);
+                Utils.drawLine(-2 + i * 10, -2 + j * 10, 11 + i * 10, 11 + j * 10);
             } else{
-                Utils.drawLine(-2 + i * 10, 10 + j * 10, 10 + i * 10, -2 + j * 10);
+                Utils.drawLine(-2 + i * 10, 11 + j * 10, 11 + i * 10, -2 + j * 10);
             }
         }
     }
--- scripts/utils.js
+++ scripts/utils.js
@@ -18,7 +18,10 @@ export function drawLine(x1, y1, x2, y2){
 
 }
 
-export function rgb(r,g,b){
+export function rgb(){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
     let rgb = "rgb(" + r + ", " + g + ", " + b + " )";
     return rgb;
 }
