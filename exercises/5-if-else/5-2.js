9cfaa6b0722929d463451f61d9a944cea310865f
Maxime Bastien <maxime.bastien@student.ehb.be>
2022-11-12 22:53:41 +0100

    5-2 maze zonder kleur

---
+++ exercises/5-if-else/5-2.js
@@ -0,0 +1,26 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+drawMaze();
+
+function drawMaze(){
+    for(let j = 0; j < height; j++){
+
+        for(let i = 0; i < width; i++){
+
+            let rand = Math.round(Math.random());
+            context.lineWidth= 5;
+            console.log(rand);
+
+            if(rand == 0){
+                Utils.drawLine(-2 + i * 10, -2 + j * 10, 10 + i * 10, 10 + j * 10);
+            } else{
+                Utils.drawLine(-2 + i * 10, 10 + j * 10, 10 + i * 10, -2 + j * 10);
+            }
+        }
+    }
+}
